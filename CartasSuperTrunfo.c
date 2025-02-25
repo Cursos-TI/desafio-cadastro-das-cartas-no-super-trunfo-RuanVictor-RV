71cde9f8d6fcc7fd1969bfb1b5ad2fca40ad6935
RuanVictor-RV <202501577083@alunos.estacio.br>
2025-02-25 17:31:58 -0300

    SuperTrunfoFinalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
+
     //variaveis carta 1
-    char estado [50];
-    char cidade [50];
+    char estado[50];
+    char cidade[50];
     char UF[5];
     int populacao;
     float areaPopulacional;
     int pontosTuristicos;
     float pib;
 
-    printf("Seja bem vindo ao jogo do super trunfo!");
-    printf("Vamos criar 2 cartas, tema: Estados do Brasil.");
-    printf("Criação da carta 1: \n");
+    printf("Seja bem vindo ao jogo do super trunfo! \n");
+    printf("Vamos criar 2 cartas, tema: Estados do Brasil. \n");
     //explicação/inicio
 
     printf("  \n");
     //separar inicio e começar carta 1
 
+    printf("\nCriação da carta 1: \n");
+
+    printf(" \n");
+    //separar
+
     printf("Digite um estado do Brasil: \n");
-    scanf("%s", estado);
+    fgets(estado, sizeof(estado), stdin);
+    estado[strcspn(estado, "\n")] = 0;
+    //fgets para ter a possibilidade de acrescentar espaços
 
     printf("Digite uma cidade deste estado: \n");
-    scanf("%s", cidade);
+    fgets(cidade, sizeof(cidade), stdin);
+    //fgets para ter a possibilidade de acrescentar espaços
+    cidade[strcspn(cidade, "\n")] = 0;
+    // Remover o '\n' extra da string
 
     printf("Digite a UF deste estado escolhido: \n");
-    scanf("%s", UF);
+    printf("Exemplo: RJ, SP, PR, SC... \n");
+    fgets(UF, sizeof(UF), stdin);
+    UF[strcspn(UF, "\n")] = 0;
+    //fgets para ter a possibilidade de acrescentar espaços
 
-    printf("Digite a quantidade de população da cidade escolhida (7/8 digitos se for milhão e 10/11 digitos se for bilhão:  \n");
+    printf("Digite a população da cidade escolhida (em número inteiro): \n");
+    printf("(Exemplo: se a população for 1 milhão, digite 1000000 e \n");
+    printf("se for 1 bilhão, digite 1000000000.)\n");
     scanf("%d",&populacao);
     //exemplo: 1234567 (numero inteiro)
-    
+
     printf("Digite a area populacional da cidade em km²(exemplo: 123.4 ou 1234.5): \n");
     scanf("%f",&areaPopulacional);
     //exemplo: 123.45 (numero com ponto(virgula))
@@ -39,20 +56,18 @@ int main() {
     scanf("%d",&pontosTuristicos);
     //exemplo: 25 (numero inteiro)
 
-    printf("Digite o pib aproximado(exemplo: 12.345(B) ou (M) 1.234.0): \n");
+    printf("Digite o pib aproximado: \n");
+    printf("(Exemplo em caso de milhão: 1.000.000, e  \n");
+    printf("em caso de bilhão: 1.000.000.000.)  \n");
     scanf("%f",&pib);
-    //exemplo: 25.3 (numero com virtula(ponto) 
+    //exemplo: 25.3 (numero com virgula(ponto)
+    
+    while(getchar() != '\n'); 
+    //corrigir erro de pergunta junto/ limpar buffer
 
-    printf("**Carta 1:** \n");
-    //apresentação da carta 1 (colocar no final 2x)
+    printf(" \n");
 
-    printf("Estado: %s\n", estado);
-    printf("Cidade: %s\n", cidade);
-    printf("UF: %s\n", UF);
-    printf("População: %d habitantes\n", populacao);
-    printf("Área Populacional: %.2f km²\n", areaPopulacional);
-    printf("Pontos Turísticos: %d\n", pontosTuristicos);
-    printf("PIB: R$ %.2f milhões\n", pib);
+    printf("\nAgora vamos para a criação da carta 2: \n");
 
     //variaveis carta 2:
     char estado1[50];
@@ -63,12 +78,78 @@ int main() {
     int pontosTuristicos1;
     float pib1;
 
-    //começo carta 2
+    printf(" \n");
+    //somente para separar
+
     printf("Digite um estado do Brasil: \n");
-    scanf("%s",&estado1);
+    fgets(estado1, sizeof(estado1), stdin);
+    estado1[strcspn(estado1, "\n")] = 0;
+    //fgets para ter a possibilidade de acrescentar espaços
 
-    //repetir as perguntas colocando 1 no final para carta 2
+    printf("Digite uma cidade deste estado: \n");
+    fgets(cidade1, sizeof(cidade1), stdin);
+    //fgets para ter a possibilidade de acrescentar espaços
+    cidade1[strcspn(cidade1, "\n")] = 0;
 
+    printf("Digite a UF deste estado escolhido: \n");
+    printf("Exemplo: RJ, SP, PR, SC... \n");
+    fgets(UF1, sizeof(UF1), stdin);
+    UF1[strcspn(UF1, "\n")] = 0;
+    //fgets para ter a possibilidade de acrescentar espaços
 
-        return 0;
-    }
+    printf("Digite a população da cidade escolhida (em número inteiro): \n");
+    printf("(Exemplo: se a população for 1 milhão, digite 1000000 e \n");
+    printf("se for 1 bilhão, digite 1000000000.)\n");
+    scanf("%d",&populacao1);
+    //exemplo: 1234567 (numero inteiro)
+    while(getchar() != '\n'); 
+    //corrigir erro de pergunta junto/ limpar buffer
+
+    printf("Digite a area populacional da cidade em km²(exemplo: 123.4 ou 1234.5): \n");
+    scanf("%f",&areaPopulacional1);
+    //exemplo: 123.45 (numero com ponto(virgula))
+    while(getchar() != '\n'); 
+    //corrigir erro de pergunta junto/ limpar buffer
+
+    printf("Digite quantos pontos turisticos tem na cidade: \n");
+    scanf("%d",&pontosTuristicos1);
+    //exemplo: 25 (numero inteiro)
+    while(getchar() != '\n'); 
+    //corrigir erro de pergunta junto/ limpar buffer
+
+    printf("Digite o pib aproximado: \n");
+    printf("(Exemplo em caso de milhão: 1.000.000, e  \n");
+    printf("em caso de bilhão: 1.000.000.000.)  \n");
+    scanf("%f",&pib1);
+    //exemplo: 25.3 (numero com virgula(ponto)
+    while(getchar() != '\n'); 
+    //corrigir erro de pergunta junto/ limpar buffer
+ 
+    //
+
+    // Dados da carta 1
+    printf("\n*Carta 1: *\n");
+    printf("Estado: %s\n", estado);
+    printf("Nome da cidade: %s\n", cidade);
+    printf("UF: %s\n", UF);
+    printf("População: %d\n", populacao);
+    printf("Área: %.1f km²\n", areaPopulacional);
+    printf("Pontos turísticos: %d\n", pontosTuristicos);
+    printf("PIB: %.1f bilhões de reais\n", pib);
+
+    // Dados da carta 2
+    printf("\n*Carta 2: *\n");
+    printf("Estado: %s\n", estado1);
+    printf("Nome da cidade: %s\n", cidade1);
+    printf("UF: %s\n", UF1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.1f km²\n", areaPopulacional1);
+    printf("Pontos turísticos: %d\n", pontosTuristicos1);
+    printf("PIB: %.1f bilhões de reais\n", pib1);
+
+    printf("  \n");
+
+    return 0;
+}
+
+//unica solução que encontrei de as perguntas não ficarem juntas foi utilizando while(getchar() != '\n'); 
